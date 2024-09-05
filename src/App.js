3a6a6deef0420f653477ec260123edb2ab695cee
Shruti <shrutibharatvhanamane@gmail.com>
2024-09-05 08:07:15 +0530

    shifted

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2>Let's start our gemini clone!</h2>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/sidebar/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './sidebar.css'
+import './img'
+const Sidebar = () => {
+  return (
+    <div classname='sidebar'>
+      <h>im sidebar!</h>
+      <div className="top">
+<img src="" alt="" />
+      </div>
+      <div className="bottom">
+
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
