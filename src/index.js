19a7a43a2aa9108336ddfd69d342196c803900bc
Shruti Vhanmane <143516390+shrutibharat-05@users.noreply.github.com>
2024-09-29 10:43:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Sidebar from './Components/sidebar/Sidebar';
-
+import Main from './Components/main/Main';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
